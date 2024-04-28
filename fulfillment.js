2f4daf4224239a6371745355ad4d85f05c54b86a
Perihane El Refai <perry.elrefai@gmail.com>
2024-04-28 20:27:22 +0300

    bh

--- fulfillment.js
+++ fulfillment.js
@@ -58,6 +58,7 @@ function makeAppointment(agent) {
     const mail = agent.parameters.email;
    //const dateTimeStart = toTimeZone(new Date(Date.parse(agent.parameters.date.split('T')[0] + 'T' + agent.parameters.time.split('T')[1].split('-')[0])), timeZone);
     const dateTimeStart = new Date(Date.parse(agent.parameters.date.split('T')[0] + 'T' + agent.parameters.time.split('T')[1].split('-')[0]));
+    dateTimeStart.setHours=dateTimeStart.getHours+1;
     const timeZone = 'Africa/Cairo'; // Set your desired time zone
     //const agentParameters = agent.parameters;
     //const dateTimeStart = parseDateTime(agentParameters.date.split('T')[0], agentParameters.time.split('T')[1].split('-')[0]);
